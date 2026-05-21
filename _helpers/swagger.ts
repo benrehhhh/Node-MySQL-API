d5b69d8c826e1f63668fc5ec2a62c8090726555e
Vin Henry S. Ardiente <vinhenrysaballaardiente@gmail.com>
2026-05-22 02:03:25 +0800

    fixed: swagger.ts

--- _helpers/swagger.ts
+++ _helpers/swagger.ts
@@ -1,12 +1,9 @@
 import express from 'express';
-import path from 'path';
+const router = express.Router();
 import swaggerUi from 'swagger-ui-express';
 import YAML from 'yamljs';
-import { Router } from 'express';
 
-const router: Router = express.Router();
-
-const swaggerDocument = YAML.load(path.join(__dirname, '../../swagger.yaml'));
+const swaggerDocument = YAML.load('./swagger.yaml');
 
 router.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
